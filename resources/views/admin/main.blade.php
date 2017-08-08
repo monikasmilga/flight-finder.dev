45280325107aba61dff1cac45e20c685d8ebf748
monikasmilga <tuuli705@gmail.com>
2017-08-08 15:32:03 +0300

    Started working on main blade and menu

--- resources/views/admin/main.blade.php
+++ resources/views/admin/main.blade.php
@@ -1,7 +1,23 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: Monika
- * Date: 8/8/2017
- * Time: 3:11 PM
- */
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <title>Flight finder</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
+    <link rel="stylesheet" href="{{asset('css/app.css')}}">
+    {{--<link rel="stylesheet" href="{{asset('css/custom.css')}}"> // if necessary in the future--}}
+</head>
+<body>
+
+@include('admin.menu')
+
+
+
+</body>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
+
+
+</html>
--- resources/views/admin/menu.blade.php
+++ resources/views/admin/menu.blade.php
@@ -1,7 +1,14 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: Monika
- * Date: 8/8/2017
- * Time: 3:12 PM
- */
+<div id="menu">
+    <h3>
+        MENU
+        {{--{{trans('app.admin-menu')}}--}}
+    </h3>
+    <ul>
+        <li><a href="{{route('app.language.index')}}">{{trans('app.language')}}</a></li>
+        <li><a href="{{route('app.menu.index')}}">{{trans('app.menu')}}</a></li>
+        <li><a href="{{route('app.categories.index')}}">{{trans('app.categories')}}</a></li>
+        <li><a href="{{route('app.pages.index')}}">{{trans('app.pages')}}</a></li>
+        <li><a href="{{route('app.users.index')}}">{{trans('app.users')}}</a></li>
+        <li><a href="{{route('app.order.index')}}">{{trans('app.orders')}}</a></li>
+    </ul>
+</div>
