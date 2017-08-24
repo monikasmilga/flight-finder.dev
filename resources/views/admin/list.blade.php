18f491725caa41a050ebf318b7abca46e302005e
monikasmilga <tuuli705@gmail.com>
2017-08-24 15:31:03 +0300

    Pagination logic fits all lists

--- resources/views/admin/list.blade.php
+++ resources/views/admin/list.blade.php
@@ -5,61 +5,62 @@
     <div class="page-content">
         @if(isset($list[0]) || isset($list['data'][0]))
 
+            <h1 class="pageTitle">{{$pageTitle}}</h1>
 
-            @if($pageTitle == 'Countries')
-                <h1 class="pageTitle">{{$pageTitle}}</h1>
+            <nav class="paginateBar" style="text-align: center">
+                <ul class="pagination">
+                    @if(($list["current_page"]) === 1)
+                        <li class="disabled-link"><a id="disabled-link">Previous</a></li>
+                    @else
+                        <li class="page-item"><a class="page-link"
+                                                 href="{{route('app.' . strtolower($pageTitle) . '.index')}}?page={{$list["current_page"]-1}}">Previous</a>
+                        </li>
+                    @endif
 
-                <nav aria-label="Page navigation example">
-                    <ul class="pagination">
-                        @if(($list["current_page"]) === 1)
-                            <li class="disabled-link"><a id="disabled-link">Previous</a></li>
+                    @for($i=1; $i<=$list['last_page']; $i++)
+                        @if($i == $list['current_page'])
+                            <li class="disabled-link"><a class="disabled-link">{{$i}}</a>
+                            </li>
                         @else
                             <li class="page-item"><a class="page-link"
-                                                     href="{{route('app.countries.index')}}?page={{$list["current_page"]-1}}">Previous</a>
+                                                     href="{{route('app.' . strtolower($pageTitle) . '.index')}}?page={{$i}}">{{$i}}</a>
                             </li>
                         @endif
+                    @endfor
 
-                        @for($i=1; $i<=$list['last_page']; $i++)
-                            <li class="page-item"><a class="page-link"
-                                                     href="{{route('app.countries.index')}}?page={{$i}}">{{$i}}</a>
-                            </li>
-                        @endfor
-
-                        @if(($list["current_page"]) === $list['last_page'])
-                            <li class="disabled-link"><a id="disabled-link">Next</a></li>
-                        @else
-                            <li class="page-item"><a class="page-link"
-                                                     href="{{route('app.countries.index')}}?page={{$list["current_page"]+1}}">Next</a>
-                            </li>
-                        @endif
-                    </ul>
-                </nav>
+                    @if(($list["current_page"]) === $list['last_page'])
+                        <li class="disabled-link"><a id="disabled-link">Next</a></li>
+                    @else
+                        <li class="page-item"><a class="page-link"
+                                                 href="{{route('app.' . strtolower($pageTitle) . '.index')}}?page={{$list["current_page"]+1}}">Next</a>
+                        </li>
+                    @endif
+                </ul>
+            </nav>
 
 
-                <table class="table table-bordered">
-                    <thead>
-                    <tr>
-                        @foreach($list['data'][0] as $key => $value)
-                            <th>{{$key}}</th>
-                        @endforeach
-                    </tr>
-                    </thead>
-                    <tbody>
-                    <tr>
-                        @foreach($list['data'] as $listItem)
-                            @foreach($listItem as $value)
-                                <td>{{$value}}</td>
-                            @endforeach
-                    </tr>
+            <table class="table table-bordered">
+                <thead>
+                <tr>
+                    @foreach($list['data'][0] as $key => $value)
+                        <th>{{$key}}</th>
                     @endforeach
+                </tr>
+                </thead>
+                <tbody>
+                <tr>
+                    @foreach($list['data'] as $listItem)
+                        @foreach($listItem as $value)
+                            <td>{{$value}}</td>
+                        @endforeach
+                </tr>
+                @endforeach
 
-                    </tbody>
-                </table>
+                </tbody>
+            </table>
 
 
-            @else
-                <h1 class="pageTitle">{{$pageTitle}}</h1>
-
+            @if($pageTitle !== 'Countries')
                 <div class="create-button">
                     <a href="{{ $route }}">
                         <button type="button" class="btn btn-secondary">
@@ -67,27 +68,6 @@
                         </button>
                     </a>
                 </div>
-
-                <table class="table table-bordered">
-                    <thead>
-                    <tr>
-                        @foreach($list[0] as $key => $value)
-                            <th>{{$key}}</th>
-                        @endforeach
-                    </tr>
-                    </thead>
-                    <tbody>
-                    <tr>
-                        @foreach($list as $listItem)
-                            @foreach($listItem as $value)
-                                <td>{{$value}}</td>
-                            @endforeach
-                    </tr>
-                    @endforeach
-
-                    </tbody>
-                </table>
-
             @endif
 
 
@@ -110,7 +90,6 @@
                 @endif
             @endif
             <h1 class="pageTitle">No data available</h1>
-
 
         @endif
     </div>
