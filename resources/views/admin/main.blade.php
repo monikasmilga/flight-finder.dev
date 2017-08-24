26a61ea31a1510dc78a789447d0aec250f21d171
monikasmilga <tuuli705@gmail.com>
2017-08-24 15:31:33 +0300

    Added glyphicons

--- resources/views/admin/main.blade.php
+++ resources/views/admin/main.blade.php
@@ -7,6 +7,7 @@
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
     <link rel="stylesheet" href="{{asset('css/app.css')}}">
     <link rel="stylesheet" href="{{asset('css/style.css')}}">
+    <script src="https://use.fontawesome.com/6abb5f0e8b.js"></script>
 </head>
 <body>
 
--- resources/views/admin/menu.blade.php
+++ resources/views/admin/menu.blade.php
@@ -1,13 +1,17 @@
 <div id="menu">
     <nav class="nav flex-column">
         <h3>MENU</h3>
-        <a class="nav-link" href="{{asset ('home') }}">Home</a>
-        <hr>
-        <a class="nav-link active" href="{{route('app.countries.index')}}">Countries</a>
-        <a class="nav-link" href="{{route('app.airports.index')}}">Airports</a>
-        <a class="nav-link" href="{{route('app.airlines.index')}}">Airlines</a>
-        <a class="nav-link" href="{{route('app.flights.index')}}">Flights</a>
-        <hr>
-        <a class="nav-link" href="{{route('app.faker.index')}}">Data faker</a>
+        <a class="nav-link" href="{{asset ('home') }}"><i class="fa fa-home" aria-hidden="true"></i> Home</a>
+        <br>
+        <a class="nav-link" href="{{route('app.faker.index')}}"><i class="fa fa-search" aria-hidden="true"></i> Search</a>
+        <br>
+        <a class="nav-link active" href="{{route('app.countries.index')}}"><i class="fa fa-globe" aria-hidden="true"></i> Countries</a>
+        <a class="nav-link" href="{{route('app.airports.index')}}"><i class="fa fa-flag" aria-hidden="true"></i> Airports</a>
+        <a class="nav-link" href="{{route('app.airlines.index')}}"><i class="fa fa-briefcase" aria-hidden="true"></i> Airlines</a>
+        <a class="nav-link" href="{{route('app.flights.index')}}"><i class="fa fa-plane" aria-hidden="true"></i> Flights</a>
+        <br>
+        <a class="nav-link" href="{{route('app.faker.index')}}"><i class="fa fa-plus-circle" aria-hidden="true"></i> Data faker</a>
     </nav>
 </div>
+
+
